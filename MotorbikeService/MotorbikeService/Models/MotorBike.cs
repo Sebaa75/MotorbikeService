19218aba6fe96f69e741a1ca436588c24c5de913
Sebastian Szymanek <sebaa75@gmail.com>
2018-04-04 20:28:43 +0200

    class added

---
+++ MotorbikeService/MotorbikeService/Models/MotorBike.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MotorbikeService.Models
+{
+    public class MotorBike
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string VIN { get; set; }
+        public string Plate { get; set; }
+        public int Mileage { get; set; }
+        public int BuildYear { get; set; }
+    }
+}
---
+++ MotorbikeService/MotorbikeService/Models/ServiceWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MotorbikeService.Models
+{
+    public class ServiceWork
+    {
+        public int Id { get; set; }
+        public DateTime date { get; set; }
+        public int Mileage { get; set; }
+        public string Comment { get; set; }
+    
+    }
+}
