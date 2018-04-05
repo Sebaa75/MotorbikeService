b63515cbbf5fc60a128344575cc1c30efe731216
Sebastian Szymanek <sebaa75@gmail.com>
2018-04-05 20:55:45 +0200

    Repositories added

---
+++ MotorbikeService/MotorbikeService/Repository/AbstractRepository.cs
@@ -0,0 +1,20 @@
+﻿using MotorbikeService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MotorbikeService.Repository
+{
+    public class AbstractRepository <T> where T: class
+    {
+        public virtual void Create(T entity)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                context.Set<T>().Add(entity);
+                context.SaveChanges();
+            }
+        }
+    }
+}
