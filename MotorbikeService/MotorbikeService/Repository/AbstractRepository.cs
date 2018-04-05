8400c0c8e3de18a6887713be8c153a72058aa840
Sebastian Szymanek <sebaa75@gmail.com>
2018-04-05 23:27:14 +0200

    Repository finished?

--- MotorbikeService/MotorbikeService/Repository/AbstractRepository.cs
+++ MotorbikeService/MotorbikeService/Repository/AbstractRepository.cs
@@ -1,7 +1,9 @@
 ﻿using MotorbikeService.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Web;
 
 namespace MotorbikeService.Repository
@@ -13,6 +15,32 @@ namespace MotorbikeService.Repository
             using (var context = new ApplicationDbContext())
             {
                 context.Set<T>().Add(entity);
+                context.SaveChanges();
+            }
+        }
+
+        public virtual void Update (T entity)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                context.Entry(entity).State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        public virtual List<T> GetWhere(Expression<Func<T, bool>> expression)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var query = context.Set<T>().Where(expression);
+                return query.ToList();
+            }
+        }
+        public virtual void Delete(T entity)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                context.Entry(entity).State = EntityState.Deleted;
                 context.SaveChanges();
             }
         }
